8ffffbcc123e5047af30e40959ded44990304409
Denis Markov <markovdenis2012@gmail.com>
2022-10-25 16:38:35 +0400

    002_var - variables

---
+++ 002_variables/circle_area.py
@@ -0,0 +1,12 @@
+
+
+pi = 3.14
+r = int(input('Enter radius of circle: '))
+
+area = pi * (r**2)
+
+print('Area: ', area, ' m**2')
+
+# r = 3
+#
+# print('Area: ', area, ' m**2')
---
+++ 002_variables/lesson.py
@@ -0,0 +1,12 @@
+hero_hp = 100
+enemy_damage = 25
+
+print('Hero hp: ', hero_hp)
+print('Enemy damage: ', enemy_damage)
+
+hero_hp = hero_hp - enemy_damage
+print('Hero hp: ', hero_hp)
+
+print( type(hero_hp) )
+hero_hp = "test"
+print( type(hero_hp) )
