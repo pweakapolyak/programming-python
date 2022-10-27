f0048b95db8fedab3d1439b904e75fcaf7770789
Denis Markov <markovdenis2012@gmail.com>
2022-10-27 22:41:05 +0400

    004_float - lesson

---
+++ 004_float/lesson.py
@@ -0,0 +1,19 @@
+import math
+
+num1 = float(input('Enter num1: '))
+num2 = float(input('Enter num2: '))
+
+print('num1+num2', num1+num2)
+print('num1-num2', num1-num2)
+print('num1*num2', num1*num2)
+print('num1/num2', num1/num2)
+
+rounded_num1 = round(num1, 3)
+
+print('round(num1, 3): ', rounded_num1)
+
+abs_num1 = abs(num1)
+print('abs(num1): ', abs_num1)
+
+cos_pi_2 = math.cos(math.pi/2)
+print(cos_pi_2)
