78ce930b1689073c2a027e50ca19a040bb5544cb
Denis Markov <markovdenis2012@gmail.com>
2022-10-27 13:33:56 +0400

    003_int - tasks

--- 003_integers/tasks/task01.py
+++ 003_integers/tasks/task01.py
@@ -14,25 +14,7 @@
 
 
 def task():
-    num_of_seconds_in_minute = 60
-    num_of_minutes_in_hour = 60
-    num_of_hour_in_day = 24
-
-    num_of_seconds_in_day = num_of_hour_in_day * num_of_minutes_in_hour * num_of_seconds_in_minute
-    num_of_seconds_in_hour = num_of_minutes_in_hour * num_of_seconds_in_minute
-
-    full_seconds = int(input('Enter num of seconds: '))
-
-    num_days = full_seconds // num_of_seconds_in_day
-    remaining_seconds = full_seconds % num_of_seconds_in_day
-
-    num_hours = remaining_seconds // num_of_seconds_in_hour
-    remaining_seconds = remaining_seconds % num_of_seconds_in_hour
-
-    num_minutes = remaining_seconds // num_of_seconds_in_minute
-    remaining_seconds = remaining_seconds % num_of_seconds_in_minute
-
-    print(num_days, num_hours, num_minutes, remaining_seconds)
+    print()
 
 
 if __name__ == "__main__":
--- 003_integers/tasks/task02.py
+++ 003_integers/tasks/task02.py
@@ -13,25 +13,7 @@
 # --------------------- Конвертер времени ---------------------
 
 def task():
-    num_of_bytes_in_kb = 1024
-    num_of_kb_in_mb = 1024
-    num_of_mb_in_gb = 1024
-
-    num_of_bytes_in_gb = num_of_mb_in_gb * num_of_kb_in_mb * num_of_bytes_in_kb
-    num_of_bytes_in_mb = num_of_kb_in_mb * num_of_bytes_in_kb
-
-    full_bytes = int(input('Enter num of bytes: '))
-
-    num_gb = full_bytes // num_of_bytes_in_gb
-    remaining_bytes = full_bytes % num_of_bytes_in_gb
-
-    num_mb = remaining_bytes // num_of_bytes_in_mb
-    remaining_bytes = remaining_bytes % num_of_bytes_in_mb
-
-    num_kb = remaining_bytes // num_of_bytes_in_kb
-    remaining_bytes = remaining_bytes % num_of_bytes_in_kb
-
-    print(num_gb, num_mb, num_kb, remaining_bytes)
+    print()
 
 
 if __name__ == "__main__":
