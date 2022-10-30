6d01be38ca3eace8a5c4e86c939dd30575c160a2
Denis Markov <markovdenis2012@gmail.com>
2022-10-30 18:40:14 +0400

    006_string

---
+++ 006_string/encoding.py
@@ -0,0 +1,16 @@
+
+
+char1 = "A"
+char2 = "a"
+
+print(char1, "=", ord(char1))
+print(char2, "=", ord(char2))
+
+char3 = "Z"
+char4 = chr(ord(char3)+32)
+
+print(char3, "=", ord(char3))
+print(char4, "=", ord(char4))
+
+str = "\tHello\n\tworld"
+print(str)
---
+++ 006_string/lesson.py
@@ -0,0 +1,16 @@
+
+
+
+str = "Hello, world! 🤯"
+
+print(str)
+
+str2 = "...."
+
+print(str + str2)
+print(str2 * 5)
+
+num = input('Enter num: ')
+print(num * 5)
+
+"5" != 5
