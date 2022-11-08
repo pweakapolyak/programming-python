b3046325e258cfc20f61af61c6afad88df7b78f3
Denis Markov <markovdenis2012@gmail.com>
2022-11-08 15:48:24 +0400

    009_bisection - task

---
+++ 009_bisection/tasks/task01.py
@@ -0,0 +1,59 @@
+# --------------------- Накопления на дом - bisection ---------------------
+
+# Условия задачи на сайте.
+
+# --------------------- Накопления на дом - bisection ---------------------
+import math
+
+
+def task():
+    portion_of_down_payment = 0.25
+    annual_investment_rate = 0.04
+    monthly_investment_rate = annual_investment_rate / 12
+
+    total_cost = int(input('Enter house total cost: '))
+    annual_salary = int(input('Enter annual salary: '))
+    num_of_years = int(input('Enter num of years: '))
+
+    monthly_salary = annual_salary / 12
+    cost_of_down_payment = total_cost * portion_of_down_payment
+
+    goal_of_months = num_of_years * 12
+    month_guess = 0
+
+    low = 0
+    high = 100
+
+    portion_saved_percent = high
+
+    is_failed = False
+
+    while month_guess != goal_of_months:
+
+        if month_guess < goal_of_months:
+            high = portion_saved_percent
+        else:
+            low = portion_saved_percent
+
+        portion_saved_percent = (low + high) / 2
+
+        month_guess = 0
+        current_savings = 0
+        while current_savings < cost_of_down_payment:
+            current_savings += current_savings * monthly_investment_rate
+            current_savings += monthly_salary * portion_saved_percent / 100
+
+            month_guess += 1
+
+        if math.isclose(portion_saved_percent, 100) and month_guess > goal_of_months:
+            is_failed = True
+            break
+
+    if is_failed:
+        print('impossible')
+    else:
+        print(portion_saved_percent)
+
+
+if __name__ == "__main__":
+    task()
---
+++ 009_bisection/tasks/test.py
@@ -0,0 +1,50 @@
+import json
+import os
+import unittest
+import task01
+import gk.assignments.assignment as assignment
+from gk.GkTest import GkTest
+
+
+class MyTestCase(GkTest):
+
+    def test_task_03(self):
+        test_set = [
+            (3_500_000, 350_000, 3),
+            (3_500_000, 350_000, 1),
+            (3_500_000, 350_000, 10),
+        ]
+        answers = {}
+        for test in test_set:
+            house_cost = test[0]
+            annual_salary = test[1]
+            num_of_years = test[2]
+            i = 0
+
+            def side_effect(s):
+                nonlocal i, house_cost, annual_salary, num_of_years
+                if i == 0:
+                    i += 1
+                    return house_cost
+                elif i == 1:
+                    i += 1
+                    return annual_salary
+                elif i == 2:
+                    i += 1
+                    return num_of_years
+
+            y = super().wrapper_with_side_effect(lambda: task01.task(), side_effect)
+            if y.rstrip().lower() != "impossible":
+                answer = super().float_answer(y.rstrip().lower(), 0)
+            else:
+                answer = "impossible"
+            answers[f"house_cost=%s_annual_salary=%s_num_of_years=%s" % test] = answer
+
+        print(json.dumps(answers))
+        self.assertEqual(assignment.check_assignment(27, 1, 'Bisection', answers), True)
+
+
+
+os.chdir('../..')
+if __name__ == '__main__':
+    unittest.main()
