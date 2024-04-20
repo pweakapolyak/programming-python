82477df66b015dae04005e9f2f9a3ba500cf404b
pweakapolyak <pweakapolyak@gmail.com>
2024-04-20 04:09:31 +0300

    task2

--- 002_variables/lesson.py
+++ 002_variables/lesson.py
@@ -10,3 +10,10 @@ print('Hero hp: ', hero_hp)
 print( type(hero_hp) )
 hero_hp = "test"
 print( type(hero_hp) )
+age_Anya = int(input('Возраст Ани: '))
+age_Sasha = int(input('Возраст Саши: '))
+print('возраст Ани: ', age_Anya)
+print('возраст Саши: ', age_Sasha)
+age_diff = age_Sasha - age_Anya
+print('Саша старше Ани на', age_diff, 'год')
+print( type(age_diff))
