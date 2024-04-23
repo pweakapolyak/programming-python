5c58f7258149bab8658b5eccc463b5a5ae949a83
pweakapolyak <pweakapolyak@gmail.com>
2024-04-23 04:22:25 +0300

    task4

--- 004_float/tasks/task01.py
+++ 004_float/tasks/task01.py
@@ -5,7 +5,12 @@
 # --------------------- Отнсоительность расстояний ---------------------
 
 def task():
-    print()
+    import math
+    l0 = float(input('Введите l0: '))
+    v = float(input('Ведите v: '))
+    c = 299792458
+    l = l0 * math.sqrt(1-(v**2)/(c**2))
+    print(round(l, 8))
 
 
 if __name__ == "__main__":
