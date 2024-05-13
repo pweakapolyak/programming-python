46e5c684b72b34646a35dd916e0b2eabc1699bd5
pweakapolyak <pweakapolyak@gmail.com>
2024-05-14 02:10:43 +0300

    task8

--- 009_bisection/tasks/task01.py
+++ 009_bisection/tasks/task01.py
@@ -7,7 +7,43 @@ import math
 
 
 def task():
-    print()
+    portion_of_down_payment = 0.25
+    annual_investment_rate = 0.04
+    monthly_investment_rate = annual_investment_rate / 12
+
+    total_cost = int(input('Price: '))
+    annual_salary = int(input('Salary: '))
+    # procent = int(input('% от 1 до 100: '))
+    goal_of_months = int(input('Months: '))
+
+    monthly_salary = annual_salary / 12
+    cost_of_down_payment = total_cost * portion_of_down_payment
+
+    month_guess = 0
+    low = 0
+    high = 100
+    portion_saved_percent = high
+    is_failed = False
+
+    while month_guess != goal_of_months:
+        if month_guess < goal_of_months:
+            high = portion_saved_percent
+        else:
+            low = portion_saved_percent
+        portion_saved_percent = (low+high)/2
+        month_guess = 0
+        current_savings = 0
+
+        while current_savings < cost_of_down_payment:
+            current_savings += current_savings * monthly_investment_rate
+            current_savings += monthly_salary * portion_saved_percent / 100
+            month_guess += 1
+        if math.isclose(portion_saved_percent, 100) and month_guess > goal_of_months:
+            is_failed = True
+            break
+    if is_failed:
+        print('Impossible')
+    else: print(portion_saved_percent)
 
 
 if __name__ == "__main__":
